b19a199ecc59b60de32d1c2707dac17dcd6a424c
Rob Pickering <rob@pickering.org>
2022-11-16 23:46:29 +0000

    Add data download

--- frontend/src/components/DataDownload.jsx
+++ frontend/src/components/DataDownload.jsx
@@ -10,19 +10,17 @@ export default function DataDownload(props) {
   function prepareData() {
     if (!dataUrl) {
       setLoading(true);
-      let json = JSON.stringify(props.data);
-      let blob = new Blob([json], { type: "text/json" });
-      setDataUrl(window.URL.createObjectURL(blob));
+      let blob = new Blob([JSON.stringify(props.data, null, 2)], { type: "application/json" });
+      console.log('download', props.data, { blob });
+      setDataUrl(URL.createObjectURL(blob));
       setLoading(false);
     }
   }
  
-
-
   return (
     <Tooltip title="prepare a download of all your data" arrow>
-      <LoadingButton href={dataUrl} onClick={prepareData} loading={loading} variant="contained" endIcon={<FileDownloadIcon />}>
-        {!dataUrl && 'Start Download'}
+      <LoadingButton href={dataUrl} download="open-follow-data.json" onClick={prepareData} loading={loading} variant="contained" endIcon={<FileDownloadIcon />}>
+        {!dataUrl && 'Download my Data'}
         {!!dataUrl && 'Download Now'}
       </LoadingButton>
     </Tooltip>
